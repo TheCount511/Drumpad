aae450b9f8d25261a25222d416b94a44f2077e0e
Fadhil <fadhil.akins@gmail.com>
2020-06-25 19:36:32 +0100

    Updates

---
+++ precache-manifest.dccc9df3bbc13071987249ac6d24205a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdd29f11b0440576fddd35a6d90c328",
+    "url": "/drumpad/index.html"
+  },
+  {
+    "revision": "702e7b7c395a8e0af685",
+    "url": "/drumpad/static/css/main.0ae2f9b5.chunk.css"
+  },
+  {
+    "revision": "5e89acba5f7d89cdd3cf",
+    "url": "/drumpad/static/js/2.20aec3f2.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/drumpad/static/js/2.20aec3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702e7b7c395a8e0af685",
+    "url": "/drumpad/static/js/main.b7cc46d5.chunk.js"
+  },
+  {
+    "revision": "e9f2be9e00c8bdff5a04",
+    "url": "/drumpad/static/js/runtime-main.f325e6c5.js"
+  }
+]);
